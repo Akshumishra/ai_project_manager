1e7269e2756af1579a04db44eeae88fac3fbb75a
rudraksh-GKMIT <rudraksh@gkmit.co>
2026-03-11 11:27:16 +0530

    feat: create base model

---
+++ src/backend/app/model/base.py
@@ -0,0 +1,25 @@
+import uuid
+from sqlalchemy import Column, DateTime, text
+from sqlalchemy.sql import func
+from sqlalchemy.dialects.postgresql import UUID
+
+from src.backend.app.db.database import Base
+
+
+class BaseModel(Base):
+    __abstract__ = True
+
+    id = Column(
+        UUID(as_uuid=True),
+        primary_key=True,
+        server_default=text("gen_random_uuid()"),
+        default=uuid.uuid4,
+    )
+
+    created_at = Column(
+        DateTime(timezone=True), server_default=func.now(), nullable=False
+    )
+    updated_at = Column(
+        DateTime(timezone=True), default=func.now(), onupdate=func.now(), nullable=False
+    )
+    deleted_at = Column(DateTime(timezone=True), nullable=True)
