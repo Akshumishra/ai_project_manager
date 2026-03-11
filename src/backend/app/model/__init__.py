8e71f4d12e78853dc13adf4c426ee06e221fb275
rudraksh-GKMIT <rudraksh@gkmit.co>
2026-03-11 16:12:26 +0530

    chores: update the __init__ file

--- src/backend/app/model/__init__.py
+++ src/backend/app/model/__init__.py
@@ -1,9 +1,9 @@
-from .users import User
+from .user import User
 from .user_detail import UserDetail
 from .project import Project, ProjectSlackDetail, ProjectMember
 from .task import Task
 from .document import Document, DocumentBlock
-from .requirement_chats import RequirementChat
+from .requirement_chat import RequirementChat
 
 __all__ = [
     "User",
