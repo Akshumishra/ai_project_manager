3b93a2388f1e23984ea43c5ee6de19c2224cc872
rudraksh-GKMIT <rudraksh@gkmit.co>
2026-03-11 11:14:19 +0530

    feat: create user details model

---
+++ src/backend/app/model/user_detail.py
@@ -0,0 +1,18 @@
+from sqlalchemy import Column, String, ForeignKey,Text
+from sqlalchemy.orm import relationship
+from sqlalchemy.dialects.postgresql import UUID
+
+from src.backend.app.model.base import BaseModel
+
+
+class UserDetail(BaseModel):
+    __tablename__ = "user_details"
+
+    user_id = Column(
+        UUID(as_uuid=True), ForeignKey("users.id"), nullable=False, unique=True
+    )
+    skills = Column(Text, nullable=True)
+    experience = Column(Text, nullable=True)
+    designation = Column(String, nullable=True)
+
+    user = relationship("User", back_populates="detail")
