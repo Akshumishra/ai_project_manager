8810e11ff256829af2259c824bc21ad10381cb8f
rudraksh-GKMIT <rudraksh@gkmit.co>
2026-03-11 12:51:21 +0530

    chores: add the __init__ file in model to make import easy

---
+++ src/backend/app/model/__init__.py
@@ -0,0 +1,18 @@
+from .users import User
+from .user_detail import UserDetail
+from .project import Project, ProjectStackDetail, ProjectMember
+from .task import Task
+from .document import Document, DocumentBlock
+from .requirment_chat import RequirementChat
+
+__all__ = [
+    "User",
+    "UserDetail",
+    "Project",
+    "ProjectStackDetail",
+    "ProjectMember",
+    "Task",
+    "Document",
+    "DocumentBlock",
+    "RequirementChat",
+]
