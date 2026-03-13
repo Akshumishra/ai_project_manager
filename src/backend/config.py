2fa1fcdb52a56a30396ba162eba24383e2eb5529
rudraksh-GKMIT <rudraksh@gkmit.co>
2026-03-13 20:57:46 +0530

    chores: add configration of mail

--- src/backend/config.py
+++ src/backend/config.py
@@ -13,6 +13,9 @@ class Config:
     REFRESH_TOKEN_EXPIRE_DAYS = os.getenv("REFRESH_TOKEN_EXPIRE_DAYS")
 
     # Email Config
-    EMAILS_FROM = os.getenv("EMAILS_FROM")
-    RESEND_API_KEY = os.getenv("RESEND_API_KEY")
+    EMAILS_FROM = os.getenv("EMAILS_FROM", "onboarding@resend.dev")
     FRONTEND_URL = os.getenv("FRONTEND_URL", "http://localhost:5173")
+    SMTP_SERVER = os.getenv("SMTP_SERVER", "smtp-relay.brevo.com")
+    SMTP_PORT = int(os.getenv("SMTP_PORT", 587))
+    SMTP_USERNAME = os.getenv("SMTP_USERNAME")
+    SMTP_PASSWORD = os.getenv("SMTP_PASSWORD")
